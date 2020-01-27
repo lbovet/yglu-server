835a1e6d55c3fab64276eb78039f5d9f573ea11a
Laurent Bovet <laurent.bovet@windmaster.ch>
2020-01-27 18:09:06 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # yglu-server
-Yglu ᕄ as a service
+Yglu ᕄ as a service for demo purposes.
+
+## Requirements
+
+``` 
+pip3 install yglu flask
+```
+
+## Run
+
+```
+FLASK_APP=yglu-server.py flask run
+```
+
+## API
+
+**`POST`** `/api/process`
+
+Request:
+
+```json
+{
+    "doc": "a: 1",                                   // (required) YAML input
+    "filename": "input.yml"                          // Filename for error reports
+}
+```
+
+Response:
+
+```json
+{
+    "doc": "a: !? ",                                 // YAML output
+    "errors": [                                      // Processing errors
+        {
+            "message": "unexpected end of input..."
+        }             
+    ]
+}
+```
---
+++ yglu-server.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, jsonify
+from yglu.main import process
+from io import StringIO
+app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 16384
+
+
+@app.route('/api/process', methods=['POST'])
+def process_doc():
+    input = request.json
+    errors = []
+    output = StringIO()
+    filename = input['filename'] if 'filename' in input else None
+    process(input['doc'], output, filename, errors)
+    if len(errors) == 0:
+        return jsonify({'doc': output.getvalue()})
+    else:
+        return jsonify({'errors': list(build_errors(errors))})
+
+
+def build_errors(errors):
+    for error in errors:
+        result = dict()
+        result['message'] = str(error)
+        yield result
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
